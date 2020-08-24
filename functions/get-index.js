98c219a6cffd8ace1d4efecaed620a9b57b41a33
Anthony Zhu <zhuangda_zhu@apple.com>
2020-08-24 10:31:59 -0400

    Add search restaurants API & cognito authentication integration

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,11 +1,16 @@
 'use strict';
 
-const co         = require("co");
-const Promise    = require("bluebird");
-const fs         = Promise.promisifyAll(require("fs"));
-const Mustache   = require('mustache')
-const http       = require('superagent-promise')(require('superagent'), Promise);
+const co        = require("co");
+const Promise   = require("bluebird");
+const fs        = Promise.promisifyAll(require("fs"));
+const Mustache  = require('mustache')
+const http      = require('superagent-promise')(require('superagent'), Promise);
+const aws4      = require('aws4');
+const URL       = require('url');
 
+const awsRegion = process.env.AWS_REGION;
+const cognitoUserPoolId = process.env.cognito_user_pool_id;
+const cognitoClientId = process.env.cognito_client_id;
 const restaurantsAPI = process.env.restaurantsAPI;
 const days = ['Sunday', 'Monday', 'Tueday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -20,14 +25,36 @@ function* loadHtml() {
 }
 
 function* getRestaurants() {
-  return (yield http.get(restaurantsAPI)).body;
+  let url = URL.parse(restaurantsAPI);
+  let opts = {
+    host: url.hostname,
+    path: url.pathname
+  };
+
+  aws4.sign(opts);
+
+  return (yield http
+    .get(restaurantsAPI)
+    .set('Host', opts.headers['Host'])
+    .set('X-Amz-Date', opts.headers['X-Amz-Date'])
+    .set('Authorization', opts.headers['Authorization'])
+    .set('X-Amz-Security-Token', opts.headers['X-Amz-Security-Token'])
+  ).body;
 }
 
 module.exports.handler = co.wrap(function* (event, context, callback) {
   let template = yield loadHtml();
   let restaurants = yield getRestaurants();
   let dayOfWeek = days[new Date().getDay()];
-  let html = Mustache.render(template, { dayOfWeek,  restaurants })
+  let view = {
+    dayOfWeek,  
+    restaurants,
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    searchUrl: `${restaurantsAPI}/search`
+  };
+  let html = Mustache.render(template, view)
   const response = {
     statusCode: 200,
     body: html,
